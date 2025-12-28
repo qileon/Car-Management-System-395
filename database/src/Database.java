93f739fa25385704dc4436fc95d286a8e9ab07b0
Siraç Süzer / qileon <137002362+qileon@users.noreply.github.com>
2025-12-28 22:56:42 +0200

    database connection class

---
+++ database/src/Database.java
@@ -0,0 +1,21 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Database {
+
+    private static final String URL =
+            "jdbc:mysql://localhost:3306/swe395carmanagementsystem";
+    private static final String USER = "root";
+    private static final String PASSWORD = "YOUR_PASSWORD";
+
+    public static Connection getConnection() {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
+}
